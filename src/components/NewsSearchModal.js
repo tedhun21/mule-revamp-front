17ab4b098c90cab990fda01e28b135fa3e852d3f
tedhun21 <songjihun940409@gmail.com>
2023-07-16 15:45:10 +0900

    Fix: modal & image

--- src/components/NewsSearchModal.js
+++ src/components/NewsSearchModal.js
@@ -34,11 +34,6 @@ export const ModalBtn = styled.button`
 export const ModalView = styled.div.attrs(() => ({
   role: "dialog",
 }))`
-  width: 800px;
-  height: 600px;
-  border-radius: 20px;
-  background: #fff;
-  margin-bottom: 200px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -46,34 +41,21 @@ export const ModalView = styled.div.attrs(() => ({
   position: relative;
   cursor: default; /* 기본 커서: 내부 클릭 이벤트 차단 */
 
+  width: 600px;
+  height: 400px;
+  border-radius: 20px;
+  background: #fff;
+
   .desc {
     display: flex;
     flex-direction: column;
-    justify-content: center;
     gap: 1rem;
-  }
-`;
+    padding-top: 50px;
+    font-size: 16px;
 
-export const ModalHeader = styled.header`
-  position: absolute;
-  top: 0;
-  width: 600px;
-  background: linear-gradient(129deg, #c240ae 0%, #775bb9 71.36%, #5966be 100%);
-  border-radius: 20px 20px 0px 0px;
-  color: white;
-  font-weight: 800;
-  font-size: 26px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-  div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  div > * {
-    padding: 10px;
+    #onsalecheck {
+      margin-left: 35px;
+    }
   }
 `;
 
@@ -81,20 +63,49 @@ export const AccordionSet = styled.div`
   display: flex;
   flex-direction: row;
   gap: 1rem;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   font-size: 18px;
 `;
 
-export const ExitBtn = styled(ModalBtn)`
-  background: white;
-  color: black;
-  width: 50px;
-  height: 50px;
-  font-size: 20px;
+export const ModalHeader = styled.header`
   position: absolute;
-  right: 0;
   top: 0;
+
+  width: 600px;
+  background: linear-gradient(129deg, #c240ae 0%, #775bb9 71.36%, #5966be 100%);
+  border-radius: 20px 20px 0px 0px;
+  color: white;
+  font-weight: 800;
+  font-size: 26px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div > * {
+    padding: 10px;
+  }
+`;
+
+export const ExitBtn = styled.button`
+  background: none;
+  color: white;
+  width: 40px;
+  height: 40px;
+  font-size: 26px;
+  border: none;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.15);
+  }
 `;
 
 export const SearchInputWrapper = styled.div`
@@ -121,7 +132,7 @@ export const SearchButton = styled.button`
   align-items: center;
   border: none;
   color: white;
-  padding: 10px;
+  padding: 2px 12px;
   background: linear-gradient(129deg, #c240ae 0%, #775bb9 71.36%, #5966be 100%);
   border-radius: 20px;
   font-size: 18px;
@@ -150,57 +161,67 @@ export default function NewsSearchModal({ news, setFilteredNews }) {
     setIsOpen(!isOpen);
   };
 
+  // category selection
   const handleChangeCatSelect = (event) => {
     setSelectCatData(event.target.value);
+  };
+
+  // search Area selction
+  const handleChangeAreaSelect = (event) => {
+    setSearchArea(event.target.value);
   };
 
   const handleInputChange = (event) => {
     setSearchValue(event.target.value);
   };
 
-  const handleSearch = () => {
-    // 둘 다 전체가 들어올때
-    if (selectCatData === "전체" && searchArea === "전체") {
-      setFilteredNews([...news]);
-    }
-
-    // CAT만 데이터가 들어올때
-    else if (selectCatData && searchArea === "전체")
-      setFilteredNews(news.filter((notice) => notice.CAT === selectCatData));
-    // AREA만 데이터가 들어올때
-    else if (selectCatData === "전체" && searchArea) {
-      if (searchArea === "제목") {
-        // title
-        setFilteredNews(
-          news.filter((notice) => notice.title.includes(searchValue)),
-        );
-      } else if (searchArea === "본문") {
-        // content
-        setFilteredNews(
-          news.filter((notice) => notice.content.includes(searchValue)),
-        );
-      } else if (searchArea === "닉네임") {
-        //author
-        setFilteredNews(news.filter((notice) => notice.author === searchValue));
+  const handleSearch = (event) => {
+    if (event.key === "Enter" || event.type === "click") {
+      // 둘 다 전체가 들어올때
+      if (selectCatData === "전체" && searchArea === "전체") {
+        setFilteredNews([...news]);
       }
-    } else {
-      setFilteredNews(
-        news
-          .filter((news) => news.CAT === selectCatData)
-          .filter((news) => {
-            if (searchArea === "닉네임") return news.author === searchValue;
-            else if (searchArea === "제목")
-              return news.title.includes(searchValue);
-            else if (searchArea === "본문")
-              return news.content.includes(searchValue);
-            return false;
-          }),
-      );
+
+      // CAT만 데이터가 들어올때
+      else if (selectCatData && searchArea === "전체")
+        setFilteredNews(news.filter((notice) => notice.CAT === selectCatData));
+      // AREA만 데이터가 들어올때
+      else if (selectCatData === "전체" && searchArea) {
+        if (searchArea === "제목") {
+          // title
+          setFilteredNews(
+            news.filter((notice) => notice.title.includes(searchValue)),
+          );
+        } else if (searchArea === "본문") {
+          // content
+          setFilteredNews(
+            news.filter((notice) => notice.content.includes(searchValue)),
+          );
+        } else if (searchArea === "닉네임") {
+          //author
+          setFilteredNews(
+            news.filter((notice) => notice.author === searchValue),
+          );
+        }
+      } else {
+        setFilteredNews(
+          news
+            .filter((news) => news.CAT === selectCatData)
+            .filter((news) => {
+              if (searchArea === "닉네임") return news.author === searchValue;
+              else if (searchArea === "제목")
+                return news.title.includes(searchValue);
+              else if (searchArea === "본문")
+                return news.content.includes(searchValue);
+              return false;
+            }),
+        );
+      }
+      setIsOpen(false);
+      setSelectCatData("전체");
+      setSearchArea("전체");
+      setSearchValue("");
     }
-    setIsOpen(false);
-    setSelectCatData("전체");
-    setSearchArea("전체");
-    setSearchValue("");
   };
 
   return (
@@ -212,7 +233,13 @@ export default function NewsSearchModal({ news, setFilteredNews }) {
         {isOpen === false ? null : (
           <ModalBackdrop>
             <ModalView onClick={handleModalClick}>
-              <ExitBtn onClick={openModalHandler}>X</ExitBtn>
+              <ModalHeader>
+                <div>
+                  <i className="fa-sharp fa-solid fa-magnifying-glass"></i>
+                  <span>검색</span>
+                </div>
+                <ExitBtn onClick={openModalHandler}>X</ExitBtn>
+              </ModalHeader>
               <div className="desc">
                 <AccordionSet>
                   <label htmlFor="category">카테고리</label>
@@ -233,7 +260,7 @@ export default function NewsSearchModal({ news, setFilteredNews }) {
                   <ModalAccordionDef
                     id="field-select"
                     value={searchArea}
-                    onChange={handleChangeCatSelect}
+                    onChange={handleChangeAreaSelect}
                   >
                     <option value="">전체</option>
                     <option value="제목">제목</option>
@@ -248,6 +275,7 @@ export default function NewsSearchModal({ news, setFilteredNews }) {
                     placeholder="Search..."
                     value={searchValue}
                     onChange={handleInputChange}
+                    onKeyDown={handleSearch}
                   />
                 </SearchInputWrapper>
                 <SearchButton onClick={handleSearch}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,89 +5,89 @@ import RecentBlock from "../components/RecentBlock";
 import axios from "axios";
 
 const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	height: 100%;
-	padding: 10px;
-	gap: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  padding: 10px;
+  gap: 10px;
 `;
 
 const Top = styled.section`
-	display: flex;
-	justify-content: space-around;
-	gap: 30px;
-	padding: 30px 0px;
-	height: 40%;
-	max-width: 1500px;
-	min-height: 240px;
-	overflow: hidden;
+  display: flex;
+  justify-content: space-around;
+  gap: 30px;
+  padding: 30px 0px;
+  height: 40%;
+  max-width: 1500px;
+  min-height: 240px;
+  overflow: hidden;
 `;
 
 const Bottom = styled(Top)`
-	height: 50%;
-	z-index: 0;
+  height: 50%;
+  z-index: 0;
 `;
 
 const Adver = styled.div`
-	border-radius: 30px;
-	box-shadow: 0px 4px 14px 0px rgba(0, 0, 0, 0.4);
-	width: 26%;
-	background: url(./images/AdImg.jpg);
-	background-position: center center;
-	background-size: cover;
+  border-radius: 30px;
+  box-shadow: 0px 4px 14px 0px rgba(0, 0, 0, 0.4);
+  width: 26%;
+  background: url(/images/AdImg.jpg);
+  background-position: center center;
+  background-size: cover;
 `;
 
 export const MainPage = styled.main`
-	width: 90%;
-	height: 100%;
-	border-radius: 30px 0px 0px 0px;
-	background: rgba(255, 255, 255, 0.8);
-	box-shadow: -4px 0px 20px 0px rgba(0, 0, 0, 0.3);
-	overflow-y: scroll;
-	padding-left: 20px;
+  width: 90%;
+  height: 100%;
+  border-radius: 30px 0px 0px 0px;
+  background: rgba(255, 255, 255, 0.8);
+  box-shadow: -4px 0px 20px 0px rgba(0, 0, 0, 0.3);
+  overflow-y: scroll;
+  padding-left: 20px;
 
-	&::-webkit-scrollbar {
-		display: none;
-	}
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 const Home = () => {
-	const [marketItems, setMarketItems] = useState([]);
-	const [noticeItems, setNoticeItems] = useState([]);
-	const [newsItems, setNewsItems] = useState([]);
+  const [marketItems, setMarketItems] = useState([]);
+  const [noticeItems, setNoticeItems] = useState([]);
+  const [newsItems, setNewsItems] = useState([]);
 
-	useEffect(() => {
-		axios
-			.get("http://localhost:3001/market")
-			.then((res) => setMarketItems(res.data.marketItems));
-		axios
-			.get("http://localhost:3001/notice")
-			.then((res) => setNoticeItems(res.data.noticeItems));
-		axios
-			.get("http://localhost:3001/news")
-			.then((res) => setNewsItems(res.data.newsItems));
-	}, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/market")
+      .then((res) => setMarketItems(res.data.marketItems));
+    axios
+      .get("http://localhost:3001/notice")
+      .then((res) => setNoticeItems(res.data.noticeItems));
+    axios
+      .get("http://localhost:3001/news")
+      .then((res) => setNewsItems(res.data.newsItems));
+  }, []);
 
-	return (
-		<MainPage>
-			<Container>
-				<Top>
-					<RecentBlock items={noticeItems} isNotice={true} />
-					<RecentBlock items={newsItems} isNotice={false} />
-					<Adver></Adver>
-				</Top>
-				<Bottom>
-					{marketItems.length > 0 ? (
-						<>
-							<Carousel items={marketItems} />
-						</>
-					) : (
-						<div>Loading...</div>
-					)}
-				</Bottom>
-			</Container>
-		</MainPage>
-	);
+  return (
+    <MainPage>
+      <Container>
+        <Top>
+          <RecentBlock items={noticeItems} isNotice={true} />
+          <RecentBlock items={newsItems} isNotice={false} />
+          <Adver></Adver>
+        </Top>
+        <Bottom>
+          {marketItems.length > 0 ? (
+            <>
+              <Carousel items={marketItems} />
+            </>
+          ) : (
+            <div>Loading...</div>
+          )}
+        </Bottom>
+      </Container>
+    </MainPage>
+  );
 };
 
 export default Home;
